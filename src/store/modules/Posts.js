faec344880fcb05edc8c2f4b1890c12a98fe8079
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 23:30:07 +0530

    Actions & mutations for add nd delete post

--- src/store/modules/Posts.js
+++ src/store/modules/Posts.js
@@ -14,11 +14,25 @@ const actions={
 
         commit('setPosts',response.data);
     },
+    async addPost({commit}, title, body){
+        const response =  await axios.post('https://jsonplaceholder.typicode.com/posts',{title,body});
+
+        console.log(title,body);
+        commit('newPost',response.data);
+        
+    },
+    async deletePost({commit}, id){
+		await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+		commit('removePost', id);
+	},
 
 }
 
 const mutations={
-    setPosts: (state, posts) => (state.posts = posts)
+    setPosts: (state, posts) => (state.posts = posts),
+    newPost: (state, post) => state.posts.unshift(post),
+    removePost: (state,id) => state.posts = state.posts.filter(post => post.id !== id),
+    
 }
 
 export default{
