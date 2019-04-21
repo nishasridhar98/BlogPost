34acaf31e43ad42cfbfdb86e0b531d47740bb06c
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 17:43:56 +0530

    Map the getters to get the posts

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
         <h3>BlogPost</h3>
+        <div class="posts">
+            <div v-for="post in allposts" :key=post.id
+             class="post">
+            {{post.title}}
+            <br>
+            {{post.body}}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+//maps getters from vuex to component
 export default {
-    name: "Post"
+    name: "Post",
+    computed: mapGetters(['allposts']),
 }
 </script>
 
