ca7b6c8a591d8da13a6072c06ebaef008cfe95a8
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 17:44:44 +0530

    Import store and include it in the vue instance

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' //root component
+import store from './store' //for vuex
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
+//render the app & mount it to the dom whose id is app
