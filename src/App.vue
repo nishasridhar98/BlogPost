d78fa5716bd9a6d4ed7adc6b04a923bbe676762a
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 16:49:15 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Posts />
+  </div>
+</template>
+
+<script>
+import Posts from './components/Posts.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Posts,
+  }
+}
+</script>
+
+<style>
+
+</style>
