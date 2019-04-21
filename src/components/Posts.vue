8c2aa1dd0581194323be9d6e2c71357b1631a542
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 19:24:19 +0530

    Style the post

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -4,23 +4,56 @@
         <div class="posts">
             <div v-for="post in allposts" :key=post.id
              class="post">
-            {{post.title}}
-            <br>
-            {{post.body}}
+            <h4>{{post.title}}</h4>
+
+            <p>{{post.body}}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-//maps getters from vuex to component
+import { mapGetters,mapActions } from 'vuex';
+//maps getters & actions from vuex to component
 export default {
     name: "Post",
+    methods: mapActions(['fetchPosts']),
     computed: mapGetters(['allposts']),
+    created(){
+        this.fetchPosts();//call the action
+    },
+    filters: {
+    text(value) {
+    //   return value.split()[:4]
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.posts {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+ 
+}
+.post {
+    
+  border: 1px solid #ccc;
+  background: #25CCF7;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+/* div.b {
+  white-space: nowrap; 
+  width: 100px; 
+  overflow: hidden;
+  text-overflow: ellipsis; 
+  border: 1px solid #000000;
+} */
 
 </style>
