4793a2917fcab5f541e64ce222752e0467650cd4
Nisha <nisha.sridhar1998@gmail.com>
2019-04-21 23:27:07 +0530

    Add the new AddPost component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
+    <AddPost />
     <Post />
   </div>
 </template>
 
 <script>
 import Post from './components/Posts.vue'
-
+import AddPost from './components/AddPost.vue'
 export default {
   name: 'app',
   components: {
     Post,
+    AddPost
   }
 }
 </script>
