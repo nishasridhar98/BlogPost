cf27227ab95d438c6198b4b58b7ab7871b3ae790
Nisha <nisha.sridhar1998@gmail.com>
2019-04-22 23:54:12 +0530

    Add FilterPost component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
+    <div class="header">
+      <h1>Blog Post</h1>
+    </div>
     <AddPost />
+    <FilterPosts />
     <Post />
   </div>
 </template>
@@ -8,15 +12,24 @@
 <script>
 import Post from './components/Posts.vue'
 import AddPost from './components/AddPost.vue'
+import FilterPosts from './components/FilterPosts.vue'
+
 export default {
   name: 'app',
   components: {
     Post,
-    AddPost
+    AddPost,
+    FilterPosts
   }
 }
 </script>
 
 <style>
+    .header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 2px;
+  }
 
 </style>
