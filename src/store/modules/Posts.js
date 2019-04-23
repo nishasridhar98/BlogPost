0dd5e1a604ff1964c8024b3d7ab84fa3a10a1c8c
Nisha <nisha.sridhar1998@gmail.com>
2019-04-23 13:21:19 +0530

    Add state,action,muatation & getters for comments

--- src/store/modules/Posts.js
+++ src/store/modules/Posts.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
 
 const state={
-    posts:[]
+    posts:[],
+    comments:[]
 };
 
 const getters={
-    allposts: (state) => state.posts
+    allposts: (state) => state.posts,
+    allcomments: (state) => state.comments
 };
 
 const actions={
     async fetchPosts({commit}){
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=100');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
 
         commit('setPosts',response.data);
     },
     async addPost({commit}, {title, body}){
         const response =  await axios.post('https://jsonplaceholder.typicode.com/posts',{title,body});
 
-        console.log(response.data);
+        // console.log(title,body);
         commit('newPost',response.data);
         
     },
@@ -28,11 +30,18 @@ const actions={
     async filterPosts({commit},e){
 		// console.log(e);
 		//Get selected number using vanilla JS
-		const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+    
 		const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
 	
 		commit('setPosts', response.data)
-	},
+    },
+    
+    async fetchComments({commit}){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/comments?_limit=10');
+
+        commit('setComments',response.data);
+    },
 
 }
 
@@ -40,7 +49,7 @@ const mutations={
     setPosts: (state, posts) => (state.posts = posts),
     newPost: (state, post) => state.posts.unshift(post),
     removePost: (state,id) => state.posts = state.posts.filter(post => post.id !== id),
-    
+    setComments: (state, comments) => (state.comments =comments),
 }
 
 export default{
