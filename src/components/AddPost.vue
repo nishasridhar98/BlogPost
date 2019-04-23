0ee0678b4705ac16ab07d60ab05a7dee731f242d
Nisha <nisha.sridhar1998@gmail.com>
2019-04-23 12:38:32 +0530

    Style the form

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -4,7 +4,9 @@
         <div class="add">
             <form @submit="onSubmit">
                 <input type="text" v-model="title" placeholder="Enter the title">
+               
                 <input type="text" v-model="body" placeholder="Enter the content">
+               
                 <br>
                 <input type="submit" value="Submit ">
             </form>
@@ -27,7 +29,7 @@ export default {
         onSubmit(e){
             e.preventDefault();//prevent refreshing of page
             this.addPost({title:this.title,body:this.body});
-            //console.log(this.title,this.body);
+            console.log(this.title,this.body);
             this.title='',//clearing the input field
             this.body=''
         }
@@ -37,25 +39,30 @@ export default {
 </script>
 
 <style>
-/* form {
-  display: flex;
-} */
+h2{
+    margin: 10px;
+}
 .add {
   border-radius: 5px;
+  margin: 10px;
   background-color: #89d7eb;
   padding: 20px;
   width: 50%;
+  border: #020708 1px solid;
 }
 input[type="text"] {
 
   width: 75%;
-  padding: 10px;
   margin-top: 10px;
   border: 1px solid #40a6c5;
   outline: 0;
-  padding: 10px 20px;
+  padding: 12px 20px;
   margin: 8px 0;
 }
+input[type=text]:focus {
+  border: 2px solid #555;
+}
+
 input[type="submit"] {
   
   background: #40a6c5;   
